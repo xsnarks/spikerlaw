f1d976c318ba4cad6bd860debd3a103a67165e1b
Agent Smith <xsnarks@gmail.com>
2025-03-07 00:32:25 +0000

    Initial upload

---
+++ old/script.js
@@ -0,0 +1,11 @@
+document.getElementById("foot01").innerHTML =
+"<p>&copy;  " + new Date().getFullYear() + " Spiker Law. All rights reserved.</p>";
+
+document.getElementById("nav01").innerHTML =
+ "<ul id='menu'>" +
+ "<li><a href='index.html'>Home</a></li>" + 
+ "<li><a href='about.html'>About</a></li>" +
+ "<li><a href='realestate.html'>Real Estate</a></li>" +
+ "<li><a href='estateplanning.html'>Estate Planning</a></li>" +
+ "<li><a href='businessorganizations.html'>Business Organizations</a></li>" +
+ "</ul>"; 
